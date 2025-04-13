134e7415cbfa427c1e2d8605ca155fa61ba10722
Gifton <mwangegifton@gmail.com>
2025-04-13 22:04:00 +0300

    loop

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const video = document.getElementById("background-video");
+
+  video.addEventListener("timeupdate", () => {
+    if (video.currentTime > video.duration - 0.1) {
+      video.currentTime = 0; // Reset the video to the start before it ends
+      video.play();
+    }
+  });
+});
